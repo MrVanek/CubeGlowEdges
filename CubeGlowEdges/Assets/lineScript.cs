9cd6db5913c4a61bdefeb109c32da3259e745423
MrVanek <vanekm@gilmour.org>
2019-03-04 08:48:02 -0500

    initialCommit

---
+++ CubeGlowEdges/Assets/lineScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineScript : MonoBehaviour
+{
+    Mesh myMesh;
+    LineRenderer lr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMesh = GetComponent<MeshFilter>().sharedMesh;
+        lr = GetComponent<LineRenderer>();
+        Vector3[] meshList = myMesh.vertices;
+        lr.positionCount = myMesh.vertexCount;
+        
+        for (int i = 0; i < myMesh.vertexCount; i++)
+        {
+            //if (i !=0 )
+            {
+                lr.SetPosition(i, new Vector3 (meshList[i].x, meshList[i].y, meshList[i].z));
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
